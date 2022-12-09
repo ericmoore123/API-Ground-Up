a0f639e508dcdca31cc740d80758910e36c47f63
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-12-09 10:19:23 -0500

    express dependencies and server setup

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-// Importing Scripts
+// Scripts imports
 const simple = require('../scripts/simple');
 
 // Testing dependencies
 const expect = require("chai").expect;
-
 // console.log(simple.multi(2,4));
 
+// ================= Simple tests imports =================
 describe('Tests of basic script functions', () => {
     describe('Test multiplication method', () => {
         it('should multiply two numbers', () => {
@@ -15,6 +15,7 @@ describe('Tests of basic script functions', () => {
             expect(simple.divide(8, 2)).to.equal(4);
         });
     });
+
     describe('Test addition and subtraction method', () => {
         it('should add two numbers', () => {
             expect(simple.add(2, 4)).to.equal(6);
