079ad1a720e36ef82b768fda7d922368210ceec8
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-12-13 12:21:47 -0500

    JS templates added, postgres functionality added 3/5

--- controller/api_controller.js
+++ controller/api_controller.js
@@ -1,42 +1,81 @@
-const dotenv = require('dotenv')
-dotenv.config({ path: __dirname + "/.env" });
+const dotenv = require('dotenv');
+dotenv.config({path: __dirname + '/.env'});
 
 const { Pool } = require('pg');
 const pool = new Pool(); // Use pool when multiple transactions are needed
 
 // Custom Imports
-const sampleTasks = require('../data/tasks').sampleTasks;
 const validateTask = require('../utils/taskVerifier').verifyTask;
 const validatePatchObject = require('../utils/taskVerifier').verifyPatchObject;
 const taskUtils = require('../utils/taskUtils')
 
-exports.getTaskById = async (req, res, next) => {
-    const taskId = parseInt(req.params.id); //string => int for comparison
-
+const taskLookup_db = async (id) => {
     const client = await pool.connect();
-    try{
-        const query_res = await client.query(`SELECT * FROM tasks WHERE id = ${taskId}`);
-        if(await query_res.rows.length == 0){
-            taskUtils.taskNotFound(taskId);
+    try {
+        const query = await client.query(`SELECT * FROM tasks WHERE id = ${id}`);
+        if (query.rows.length === 0){
+            return false;
         }else{
-            res.send(JSON.stringify(query_res.rows));
+            return query;
         };
-    }catch(err) {
-        console.error(err);
+    }catch(err){
+        return console.error(err);
+    }finally {
+        client.end();
     };
 };
 
-exports.createNewTask = (req, res, next) => {
+exports.getTasks = async (req, res, next) => {
+    const client = await pool.connect();
+    try{
+        const tasks = await client.query('SELECT * FROM tasks');
+        // res.send(JSON.stringify(tasks.rows));
+        res.render('pages/index', {
+            tasks: tasks.rows
+        });
+    }catch(err){
+        console.error(err)
+    }finally {
+        client.end();
+    };
+};
+
+exports.getTaskById = async (req, res, next) => {
+    const taskId = parseInt(req.params.id); //string => int for comparison
+
+    try{
+        const searchTask = await taskLookup_db(taskId);
+        if(await searchTask == false ) {
+            res.status(404).send(`Task with id: ${taskId} not found.`);
+        }else{
+            res.send(JSON.stringify(searchTask.rows));
+        }
+    }catch(err) {
+        console.error(err);
+    }finally {
+        client.end();
+    };
+};
+
+exports.createNewTask = async (req, res, next) => {
     const { error } = validateTask(req.body); // Validate formatting of new task
     if (error) return res.status(400).send(error.message);
 
-    const task = {
-        id: sampleTasks.length + 1,
-        name: req.body.name,
-        completed: req.body.completed
+    const client = await pool.connect();
+    try{
+        const lastElQuery = await client.query('SELECT * FROM tasks ORDER BY id DESC LIMIT 1');
+        const new_Id = lastElQuery.rows[0].id + 1;
+        
+        const queryText = "INSERT INTO tasks (id, name, completed) VALUES ($1, $2, $3)";
+        const queryVals = [new_Id, req.body.name, req.body.completed];
+        await client.query(queryText, queryVals);
+
+        res.status(201).send(`New DB entry with id: ${new_Id} created successfully!`);
+    }catch(err){
+        console.error(err);
+    }finally {
+        client.end();
     };
-    sampleTasks.push(task);
-    res.status(201).send(task);
 };
 
 exports.replaceTaskById = (req, res, next) => {
@@ -64,14 +103,21 @@ exports.modifyTaskById = (req, res, next) => {
     res.send(task);
 };
 
-exports.deleteTask = (req, res, next) => {
-    const task = taskUtils.findTask(parseInt(req.params.id));
-    if (!task) taskUtils.taskNotFound(taskId);
+exports.deleteTask = async (req, res, next) => {
+    const taskId = parseInt(req.params.id);
 
-    // Delete task with sent id
-    newTasks = sampleTasks.filter((task) => {
-        return task.id !== parseInt(req.params.id);
-    });
-
-    res.send(newTasks);
+    const client = await pool.connect();
+    try{
+        const query = await taskLookup_db(taskId);
+        if(await query == false){
+            res.status(404).send(`Task with id: ${taskId} not found.`);
+        }else{
+            const del = await client.query(`DELETE FROM tasks WHERE id = ${taskId}`);
+            res.send(`${del.rowCount} DB record(s) with id ${taskId} deleted successfully!`);
+        };
+    }catch(err){
+        console.error(err);
+    }finally {
+        client.end();
+    };
 };
--- server.js
+++ server.js
@@ -2,18 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const sampleTasks = require('./data/tasks').sampleTasks;
 
 // Controller
 const controller = require('./controller/api_controller');
 
 const PORT = process.env.PORT || 8800;
 app.use(express.json());
+app.set('view engine', 'ejs');
 
 // ========== API Routes ========== 
-app.get('/api/tasks', (req, res, next) => {
-    res.send(sampleTasks);
-});
+app.get('/api/tasks', controller.getTasks);
 
 app.get('/api/tasks/:id', controller.getTaskById);
 app.post('/api/tasks', controller.createNewTask);
@@ -23,7 +21,6 @@ app.put('/api/tasks/:id', controller.replaceTaskById);
 app.patch('/api/tasks/:id', controller.modifyTaskById);
 app.delete('/api/tasks/:id', controller.deleteTask);
 
-const { connect_Db } = require('./db/db_postgres');
 app.listen(PORT, () => {
     // connect_Db();
     console.log('Server running on port: ', PORT);
