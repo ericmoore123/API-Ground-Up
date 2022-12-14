25de97cb051c3319b86262a07e159ffda178c58d
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-12-13 22:07:21 -0500

    Crud operations complete for postgres database schema

--- controller/api_controller.js
+++ controller/api_controller.js
@@ -7,7 +7,7 @@ const pool = new Pool(); // Use pool when multiple transactions are needed
 // Custom Imports
 const validateTask = require('../utils/taskVerifier').verifyTask;
 const validatePatchObject = require('../utils/taskVerifier').verifyPatchObject;
-const taskUtils = require('../utils/taskUtils')
+// const taskUtils = require('../utils/taskUtils');
 
 const taskLookup_db = async (id) => {
     const client = await pool.connect();
@@ -29,8 +29,7 @@ exports.getTasks = async (req, res, next) => {
     const client = await pool.connect();
     try{
         const tasks = await client.query('SELECT * FROM tasks');
-        // res.send(JSON.stringify(tasks.rows));
-        res.render('pages/index', {
+        res.status(200).render('pages/index', {
             tasks: tasks.rows
         });
     }catch(err){
@@ -42,7 +41,7 @@ exports.getTasks = async (req, res, next) => {
 
 exports.getTaskById = async (req, res, next) => {
     const taskId = parseInt(req.params.id); //string => int for comparison
-
+    const client = await pool.connect();
     try{
         const searchTask = await taskLookup_db(taskId);
         if(await searchTask == false ) {
@@ -78,29 +77,60 @@ exports.createNewTask = async (req, res, next) => {
     };
 };
 
-exports.replaceTaskById = (req, res, next) => {
-    const task = taskUtils.findTask(parseInt(req.params.id));
-    if (!task) taskUtils.taskNotFound(taskId);
-    const { error } = validateTask(req.body);
-    if (error) return res.status(400).send(error.message);
+// Only accepts validated task objects
+exports.replaceTaskById = async (req, res, next) => {
+    const taskId = parseInt(req.params.id); 
+    const client = await pool.connect();
+    try{
+        const searchTask = await taskLookup_db(taskId);
+        if(await searchTask == false ) {
+            res.status(404).send(`Task with id: ${taskId} not found.`);
+        }else{
+            const { error } = validateTask(req.body);
+            if (error) return res.status(400).send(error.message);
 
-    task.name = req.body.name;
-    task.completed = req.body.completed;
+            const queryText = `UPDATE "tasks" SET "name" = $1, "completed" = $2 WHERE "id" = $3`;
+            const queryVals = [req.body.name, req.body.completed, taskId];
+            const query = await client.query(queryText, queryVals);
 
-    res.send(task);
+            // If query returns "No rows effected" send error
+            if(await query.rowCount === 0) return res.status(400).send(`No database entries effected!`);
+
+            res.status(200).send(`Task with id: ${taskId} replaced successfully!`);
+        };
+    }catch(err){
+        console.error(err);
+    }finally{
+        client.end();
+    };
 };
 
 // Accepts custom user objects
-exports.modifyTaskById = (req, res, next) => {
-    const task = taskUtils.findTask(parseInt(req.params.id));
-    if (!task) taskUtils.taskNotFound(taskId);
-    const { error } = validatePatchObject(req.body);
-    if (error) return res.status(400).send(error.message);
+exports.modifyTaskById = async (req, res, next) => {
+    const taskId = parseInt(req.params.id); 
+    const client = await pool.connect();
+    try{
+        const searchTask = await taskLookup_db(taskId);
+        if(await searchTask == false ) {
+            res.status(404).send(`Task with id: ${taskId} not found.`);
+        }else{
+            const { error } = validatePatchObject(req.body);
+            if (error) return res.status(400).send(error.message);
 
-    if (req.body.name) task.name = req.body.name;
-    if (req.body.completed) task.completed = req.body.completed;
-
-    res.send(task);
+            // Loop over body params to update task data
+            for(let [key, val] of Object.entries(req.body)){
+                const queryText = `UPDATE "tasks" SET "${key}" = $1 WHERE "id" = $2`;
+                const queryVals = [val, taskId];  
+                const query = await client.query(queryText, queryVals);
+                if(await query.rowCount === 0) return res.status(400).send(`No database entries effected!`);
+            };
+            res.status(200).send(`Field(s) in task with id: ${taskId} updated successfully!`);
+        };
+    }catch(err){
+        console.error(err);
+    }finally{
+        client.end();
+    };
 };
 
 exports.deleteTask = async (req, res, next) => {
@@ -113,6 +143,7 @@ exports.deleteTask = async (req, res, next) => {
             res.status(404).send(`Task with id: ${taskId} not found.`);
         }else{
             const del = await client.query(`DELETE FROM tasks WHERE id = ${taskId}`);
+            // console.log(del);
             res.send(`${del.rowCount} DB record(s) with id ${taskId} deleted successfully!`);
         };
     }catch(err){
